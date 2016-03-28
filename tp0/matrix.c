4ee008d0ff3cf2f904157fe8b3a7f82b05c04eea
mnforlenza <mnforlenza@gmail.com>
2016-03-27 22:53:20 -0300

    First multiplication version

--- tp0/matrix.c
+++ tp0/matrix.c
@@ -2,27 +2,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double double_at(matrix_t* m, size_t i, size_t j){
+	return (double)*(m->array+(i*m->cols+j));
+}
+
+void load_value(matrix_t* m, size_t i, size_t j, double value){
+	m->array[i*m->cols+j]=value;
+
+}
+
 matrix_t* create_matrix(size_t rows, size_t cols){
 	matrix_t* matrix = (matrix_t*)malloc(sizeof(matrix_t));
 	matrix->rows = rows;
 	matrix->cols = cols;
+	size_t array_size = sizeof(double)*(matrix->cols)*(matrix->rows);
+	matrix->array = (double*)malloc(array_size);
 	return matrix;
 }
 
 int print_matrix(FILE* fp, matrix_t* m){
 	printf("print_matrix");
+	return 0;
 }
 
-matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2){
-	matrix_t* res = (matrix_t*)malloc(sizeof(matrix_t));
-/*	for (int x = 0; x < m1->rows; ++x) {
-
-	}*/
-	return res;
+matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2) {
+	matrix_t* result = create_matrix(m1->rows, m2->cols);
+	size_t i = 0;
+	size_t j = 0;
+	double aux = 0.0;
+	size_t x;
+	for (; i < m1->rows; i++) {
+		for (j = 0; j < m1->cols; j++) {
+			aux = 0.0;
+			for(x=0; x<m1->cols;x++){
+				double first = double_at(m1,i,x);
+				double second = double_at(m2,x,j);
+				aux += (first*second);
+			}
+			load_value(result,i,j,aux);
+		}
+	}
+	return result;
 }
 
 void destroy_matrix(matrix_t* m){
 	free(m);
 }
 
+void print_matrix_std_o(matrix_t* m) {
+	size_t i = 0;
+	size_t j = 0;
+	for (; i < m->rows; i++) {
+		for (j=0; j < m->cols; j++) {
+			printf("%f ", double_at(m,i,j));
+		}
+		printf("\n");
+	}
+}
 
