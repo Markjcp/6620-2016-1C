538431366336d26881397c29dcfc9f6f1fdd9c90
federicobalina <federicobalina@gmail.com>
2016-04-25 15:38:53 -0300

    Fix errores de entrega TP0 (crecimiento de buffer exponencial y checkeo de EOF como int)

--- tp1/lectura.c
+++ tp1/lectura.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <lectura.h>
-#define TAMANIO_INICIAL_BUFFER 10
 #define FIN_LINEA '\n'
 #define FIN_STRING '\0'
+#define TAMANIO_INICIAL_BUFFER 10
 
 
 char* leer_linea(){
-	char c;	
+	int c;
+	char ch;	
 	size_t tam = TAMANIO_INICIAL_BUFFER; 
 	int i;
+	int exp_realloc = 1;
 
 	char* buffer = malloc(sizeof(char)*(tam));
 	if (!buffer) return NULL;
 	c = getchar();
-	for(i = 0; c != FIN_LINEA ; i++){ //leo hasta el enter
+	ch = c;
+	for(i = 0; ch != FIN_LINEA ; i++){ //leo hasta el enter
 		if (c == EOF)
 		{	
 			free(buffer);
 			return NULL;
 		}
 		if(i + 1 == tam){ // si llego al tamanio de mi cadena
-			tam += tam;  // la forma de modificar tam es arbitraria
+			exp_realloc += 1;
+			tam = (size_t)pow(TAMANIO_INICIAL_BUFFER,exp_realloc);  // la forma de modificar tam es arbitraria
 			char* aux = realloc(buffer, tam * sizeof(char));
 			if(!aux) return NULL;  // si falla realloc, termino aca
 			buffer = aux;
 		}
 
-		buffer[i] = c;
+		buffer[i] = ch;
 		c = getchar();
+		ch = c;
 	}
 	buffer[i] = FIN_STRING; // es necesario cerrar el string
 	return buffer;
