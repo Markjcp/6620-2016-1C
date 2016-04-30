524402053029b7ae4083b3d8ebdcd030ab1ae312
federicobalina <federicobalina@gmail.com>
2016-04-30 17:18:03 -0300

    Fix bug: llamada directa al syscall

--- tp1/print_string.c
+++ tp1/print_string.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <print_string.h>
 
 ssize_t print_string(int fd, char* str){
 	ssize_t result = 0;
-	FILE* fp = fdopen(fd, "a+");
-	if(!fp)
-	{
-	  return ERROR;
-	}
-	result = fprintf(fp,str);
-	fclose(fp);
+	result = write(fd,str, strlen(str));
 	if(result < 0)return ERROR;
 	return result;
 }
