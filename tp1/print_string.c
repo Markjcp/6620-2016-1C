b32f9bd4b72e46dc44592c1c54ef5354c151ab74
veze <masrianv@gmail.com>
2016-04-30 12:15:46 -0300

    Need to test print_string C functionality

--- tp1/print_string.c
+++ tp1/print_string.c
@@ -4,5 +4,12 @@
 
 ssize_t print_string(int fd, char* str){
 	ssize_t result = 0;
+	FILE* fp = fdopen(fd, "a+");
+	if(!fp)
+	{
+	  return ERROR;
+	}
+	result = fprintf(fp,str);
+	fclose(fp);
 	return result;
 }
--- tp1/print_string.h
+++ tp1/print_string.h
@@ -1,6 +1,8 @@
 #ifndef PRINT_STRING_H_
 #define PRINT_STRING_H_
 
+#define ERROR -1
+
 #include <stdlib.h>
 
 ssize_t print_string(int fd, char* str);
