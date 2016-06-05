4d78e85cfeb3b5cddf76b46e1895e37e7c08c6e5
veze <masrianv@gmail.com>
2016-06-05 15:57:37 -0300

    Inicio TP2

---
+++ tp2/matrix.h
@@ -0,0 +1,27 @@
+#ifndef MATRIX_H_
+#define MATRIX_H_
+#define ERROR_ESCRITURA -1
+
+#include <stdlib.h>
+
+typedef struct matrix {
+	size_t rows;
+	size_t cols;
+	double* array;
+} matrix_t;
+
+matrix_t* create_matrix(size_t rows, size_t cols);
+
+int print_matrix(FILE* fp, matrix_t* m);
+
+int print_matrix_std_o(matrix_t* m);
+
+//matrix_t* matrix_multiply(matrix_t* m1, matrix_t* m2);
+void matrix_multiply(matrix_t* m1, matrix_t* m2,matrix_t* mr, int bs);
+
+void destroy_matrix(matrix_t* m);
+
+void load_value(matrix_t* m, size_t i, size_t j, double value);
+
+
+#endif /* MATRIX_H_ */
